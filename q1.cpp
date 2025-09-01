987a91b5238350150fb85e1f8bd31abaec2c7dc3
kishorekishore34885-tech <kishorekishore34885@gmail.com>
2025-09-01 11:59:24 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("welcome");
+	return 0;
+}
---
+++ q3.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+char main();
+{
+    int str[8];
+	scanf("%s",str);
+	printf("%s",str);
+	return 0;
+}
